8d7ef1f73f962efe65a677fd7c7ab90bec468500
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2011-08-19 08:50:40 +0000

    Inline documentation correction

--- trunk/lib/WAYF/Logger/FileLogger.php
+++ trunk/lib/WAYF/Logger/FileLogger.php
@@ -28,8 +28,8 @@
  * @author     Jacob Christiansen <jach@wayf.dk>
  * @copyright  Copyright (c) 2011 Jacob Christiansen, WAYF (http://www.wayf.dk)
  * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @version    SVN: $Id$
- * @link       SVN: $URL$
+ * @version    $Id$
+ * @link       $URL$
  */
 
 /**

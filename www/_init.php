06f77843b1083f20e2d648eaa33608ebfa507f94
jach <jach@wayf.dk>
2011-08-19 08:35:54 +0000

    Initial commit including autoloader and bootstrap

---
+++ www/_init.php
@@ -0,0 +1,36 @@
+<?php
+// Define the root path of JAKOB 
+define('ROOT', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+
+// System status
+define('STATUS', 'development');
+
+// Set error logging
+ini_set('log_errors', TRUE);
+ini_set('error_log', ROOT . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR . 'jakob_error.log');
+
+// Check what status we have
+switch (STATUS) {
+    case 'production': {
+        ini_set('display_errors', 'off');
+        ini_set('display_startup_errors', FALSE);
+        break;
+    }
+    case 'development': {
+        // Display all errors in development
+        ini_set('display_errors', 'on');
+        ini_set('display_startup_errors', TRUE);
+        ini_set('error_reporting', -1);
+        break;
+    }
+    default: {
+        die('Application status not set. Terminating execution.');
+    }
+}
+
+// Include the autoloader
+include ROOT . 'lib' . DIRECTORY_SEPARATOR . 'SplClassLoader.php';
+
+// Register all classes under WAYF
+$classLoader = new SplClassLoader('WAYF', ROOT . 'lib');
+$classLoader->register();

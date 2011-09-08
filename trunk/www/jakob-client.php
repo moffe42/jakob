28159ed409c80b71534d1e30976a93daf103954b
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2011-09-08 09:26:35 +0000

    Development

--- trunk/www/jakob-client.php
+++ trunk/www/jakob-client.php
@@ -2,6 +2,7 @@
 $strat = microtime(true);
 include '_init.php';
 
+/*
 $client = new \WAYF\Connector\Client\JakobClient();
 
 $res = $client->doAsync('GetUser', '{"cpr":"1302822111"}');
@@ -15,3 +16,82 @@ $end = microtime(true);
 var_dump($result);
 var_dump($end - $strat);
 die();
+ */
+
+
+$jobs2 = array(
+    'tasks' => array(
+        array(
+            '_id' => 'GetUser',    
+            '_priority' => 'sync',
+            '_options' => array(
+                'userkey' => 'wayf',        
+                'key' => 'wayf4ever',        
+            ),
+        ),
+    ),
+);
+
+
+$jobs = array(
+    'tasks' => array(
+        array(
+            '_id' => 'CPR1',    
+            '_priority' => 'sync',
+            '_options' => array(
+                'uid' => '1302822111',        
+            ),
+            '_inattr' => array(
+                'cpr'
+            ),
+            '_outattr' => array(
+                'knr'
+            ),
+        ),
+        array(
+            '_id' => 'VIP2',    
+            '_priority' => 'sync',
+            '_options' => array(
+                'uid' => '1302822111',        
+            ),
+        ),
+        array(
+            '_priority' => 'batch',
+            'tasks' => array(
+                array(
+                    '_id' => 'CPR3',    
+                    '_priority' => 'async',
+                    '_options' => array(
+                        'uid' => '1302822111',        
+                    ),
+                ),
+                array(
+                    '_id' => 'VIP4',    
+                    '_priority' => 'async',
+                    '_options' => array(
+                        'uid' => '1302822111',        
+                    ),
+                ),
+                array(
+                    '_id' => 'VIP5',    
+                    '_priority' => 'sync',
+                    '_options' => array(
+                        'uid' => '1302822111',        
+                    ),
+                ),
+            ),    
+        ),
+    ),    
+);
+
+$client = new \WAYF\Connector\Client\TestClient();
+$client = new \WAYF\Connector\Client\JakobClient();
+
+$logger = new \WAYF\Logger\SysLogger();
+$logger->log(LOG_ERR, "TESTER");
+
+$attr_col = new \WAYF\AttributeCollector();
+$attr_col->setLogger($logger);
+$attr_col->setClient($client);
+
+var_dump($attr_col->processTasks($jobs2['tasks'], array('cpr' => '1302822111')));

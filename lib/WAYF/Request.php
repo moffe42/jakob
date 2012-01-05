b42108864f3013c397785746da87a7e87ff4806c
jach <jach@wayf.dk>
2012-01-05 10:02:25 +0000

    Require signature on requests to JAKOB

--- lib/WAYF/Request.php
+++ lib/WAYF/Request.php
@@ -11,6 +11,12 @@ class Request
     private $_returnParams = '';
     private $_returnMethod = 'post';
     private $_options = '';
+    private $_config = array();
+
+    public function __construct($config)
+    {
+        $this->_config = $config;
+    }
 
     public function handleRequest()
     {
@@ -46,10 +52,41 @@ class Request
                 $data = $_POST;        
         }  
 
+        // Validate signature on request
+        if (!isset($data['consumerkey'])) {
+            throw new RequestException('Consumer key not set on request');
+        }
+        
+        if (!isset($data['signature'])) {
+            throw new RequestException('Signature not set on request');
+        }
+
+        try {
+            $consumer = new \WAYF\Consumer($this->_config);
+            $consumer->consumerkey = $data['consumerkey'];
+            $consumer->load();
+        } catch(\WAYF\ConsumerException $e) {
+            throw new RequestException($e->getMessage());
+        }
+
+        $signparams = $data;
+        unset($signparams['consumerkey']);
+        unset($signparams['signature']);
+
+        $signer = new \WAYF\Security\Signer\GetRequestSigner();
+        $signer->setUp($consumer->consumersecret, $signparams);
+
+        if (!$signer->validate($data['signature'])) {
+            throw new RequestException('Signature on request is not valid');
+        }
+
+        // Grab the attributes
         $this->_attributes = (isset($data['attributes']) && !empty($data['attributes'])) ? json_decode($data['attributes'], true) : array();  
         if ((json_last_error() != JSON_ERROR_NONE ) && is_null($this->_attributes)) {
             throw new RequestException('Attributes - ' . JsonHelper::errornoToString(json_last_error()));
         }
+
+        // Grab return URL parameter
         if (!isset($data['returnURL']) || !($this->_returnURL = urldecode($data['returnURL']))) {
             throw new RequestException('No return URL found');
         }
@@ -59,15 +96,21 @@ class Request
             throw new RequestException('Supplied return URL is not valid');
         }
          */
+        // Grab optional return parameters
         $this->_returnParams = (isset($data['returnParams']) && !empty($data['returnParams'])) ? json_decode($data['returnParams'], true) : array();
         if ((json_last_error() != JSON_ERROR_NONE ) && is_null($this->_returnParams)) {
             throw new RequestException('Return parameters - ' . JsonHelper::errornoToString(json_last_error()));
         }
+
+        // Get return method
         $this->_returnMethod = isset($data['returnMethod']) ? $data['returnMethod'] : 'post';  
+
+        /* 
         $this->_options = (isset($data['options']) && !empty($data['options'])) ? json_decode($data['options'], true) : array();
         if ((json_last_error() != JSON_ERROR_NONE ) && is_null($this->_options)) {
             throw new RequestException('Options - ' . JsonHelper::errornoToString(json_last_error()));
         }
+         */
 
         return $this; 
     }

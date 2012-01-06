6cfb2eac100f16946a1089131cb1dec8b48fd605
jach <jach@wayf.dk>
2012-01-06 11:23:51 +0000

    Added generic header and footer templates for reuse

---
+++ templates/footer.tpl.php
@@ -0,0 +1,10 @@
+        <hr />
+        <address>
+            WAYF - Where Are You From<br />
+            H. C. Andersens Boulevard 2<br />
+            DK-1553 København V<br />
+            Web: <a href="http://www.wayf.dk">www.wayf.dk</a><br />
+            E-mail: <a href="mailto:sekretariat@wayf.dk">sekretariat@wayf.dk</a>
+        </address>
+    </body>
+</html>
---
+++ templates/header.tpl.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>JAKOB - Attribute collector by WAYF</title>
+        <meta charset="utf-8" />
+        <meta name="application-name" content="JAKOB" />
+        <meta http-equiv="Cache-Control" content="no-cache" />
+        <meta http-equiv="expires" content="Mon, 22 Jul 2002 11:12:01 GMT" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta name="robots" content="none" />
+        <link href="/style/jakob.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>

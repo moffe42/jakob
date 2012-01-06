9f97ee84ef4cc8724fcd499401d07dde7517781c
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2012-01-06 11:23:51 +0000

    Added generic header and footer templates for reuse

--- trunk/templates/error.tpl.php
+++ trunk/templates/error.tpl.php
@@ -1,33 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>JAKOB - Attribute collector by WAYF</title>
-        <meta charset="utf-8" />
-        <meta name="application-name" content="JAKOB" />
-        <meta http-equiv="Cache-Control" content="no-cache" />
-        <meta http-equiv="expires" content="Mon, 22 Jul 2002 11:12:01 GMT" />
-        <meta http-equiv="pragma" content="no-cache" />
-        <meta name="robots" content="none" />
-    </head>
-    <body>
-        <h1>Error</h1>
-        <?php
-        if (isset($errortitle) ) {
-            echo '<h2>' . $errortitle . '</h2>';
-        }
-        if (isset($errormsg) ) {
-            echo '<p>' . $errormsg . '</p>';
-        } else {
-            echo '<p>An error occured. Please contact the system administrator.</p>';
-        }
-        ?>
-        <hr />
-        <address>
-            WAYF - Where Are You From<br />
-            H. C. Andersens Boulevard 2<br />
-            DK-1553 København V<br />
-            Web: <a href="http://www.wayf.dk">www.wayf.dk</a><br />
-            E-mail: <a href="mailto:sekretariat@wayf.dk">sekretariat@wayf.dk</a>
-        </address>
-    </body>
-</html>
+<?php
+// Include standard header
+include 'header.tpl.php';
+
+echo "<h1>Error</h1>";
+if (isset($errortitle) ) {
+    echo '<h2>' . $errortitle . '</h2>';
+}
+if (isset($errormsg) ) {
+    echo '<p>' . $errormsg . '</p>';
+} else {
+    echo '<p>An error occured. Please contact the system administrator.</p>';
+}
+
+// Include standard footer
+include 'footer.tpl.php';
+?>
--- trunk/templates/timeout.tpl.php
+++ trunk/templates/timeout.tpl.php
@@ -1,28 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>JAKOB - Attribute collector by WAYF</title>
-        <meta charset="utf-8" />
-        <meta name="application-name" content="JAKOB" />
-        <meta http-equiv="Cache-Control" content="no-cache" />
-        <meta http-equiv="expires" content="Mon, 22 Jul 2002 11:12:01 GMT" />
-        <meta http-equiv="pragma" content="no-cache" />
-        <meta name="robots" content="none" />
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    </head>
-    <body>
+<?php
+include 'header.tpl.php';
+?>
         <h1>Time out</h1>
         <form method="post" action="">
         <input type="hidden" name="token" value="<?php echo $token; ?>" />
             <input type="submit" value="Submit" />
         </form>
-        <hr />
-        <address>
-            WAYF - Where Are You From<br />
-            H. C. Andersens Boulevard 2<br />
-            DK-1553 København V<br />
-            Web: <a href="http://www.wayf.dk">www.wayf.dk</a><br />
-            E-mail: <a href="mailto:sekretariat@wayf.dk">sekretariat@wayf.dk</a>
-        </address>
-    </body>
-</html>
+<?php
+include 'footer.tpl.php';
+?>

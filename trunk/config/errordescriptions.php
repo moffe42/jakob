35f105c3ab3a162ef6c7444670eff119e71c1cdb
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2012-01-09 10:06:30 +0000

    Discard error-pedia

--- trunk/config/errordescriptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-$errordescriptions = array(
-    1 => array(
-        'title' => 'Generic error',
-        'description' => 'This is a generic error. This error is displayed when the actual error have not yet been categorised.',    
-    ),   
-);

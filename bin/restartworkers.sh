d698a9b6dae418c98c1e1c384c912b798f876593
jach <jach@wayf.dk>
2012-01-09 11:32:47 +0000

    Update to restart script

--- bin/restartworkers.sh
+++ bin/restartworkers.sh
@@ -3,7 +3,9 @@
 echo -e "Restarting all JAKOB workers\n"
 
 echo -e "The following workers are killed\n"
+echo -n -e "\E[31;40m"
 pgrep -l -f connector-worker.php
+echo -n -e "\x1b[39;49;00m"
 
 echo -e "\nKilling all JAKOB workers\n"
 pkill -f connector-worker.php
@@ -12,4 +14,6 @@ echo -e "Starting all JAKOB workers again\n"
 ./jakob-worker.php
 
 echo -e "\nThe following workers are now running\n"
+echo -n -e "\E[32;40m"
 pgrep -l -f connector-worker.php
+echo -n -e "\x1b[39;49;00m"

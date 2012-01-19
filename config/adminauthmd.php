af075403a14058ca335e4f4b7e23907c7f4c6ab1
jach <jach@wayf.dk>
2012-01-19 09:45:37 +0000

    Update to admin auth functionallity

---
+++ config/adminauthmd.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+    'idp_certificate' => 'INSERT IDP PUBLIC CERTIFICATE', 
+    'sso' => 'INSER IDP SSO ENDPOINT',
+    'sp_key' => "INSERT OWN PRIVATE KEY",
+    'asc' => 'http://<OWN DOMAIN>/admin/',
+    'sp' => 'http://<OWN DOMAIN>/admin/'
+);

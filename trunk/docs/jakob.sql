6c4d12be97a78cade11ec81946f294338bd5c324
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2012-01-23 13:54:42 +0000

    Update to SQL script

--- trunk/docs/jakob.sql
+++ trunk/docs/jakob.sql
@@ -1,11 +1,5 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-
 CREATE TABLE `jakob__configuration` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `jobid` varchar(40) CHARACTER SET utf8 COLLATE utf8_danish_ci NOT NULL,
@@ -14,5 +8,12 @@ CREATE TABLE `jakob__configuration` (
       `targetidp` text CHARACTER SET utf8 COLLATE utf8_danish_ci NOT NULL,
       `configuration` text CHARACTER SET utf8 COLLATE utf8_danish_ci NOT NULL,
       PRIMARY KEY (`id`),
-      UNIQUE KEY `jobid` (`jobid`)
+      KEY `jobid` (`jobid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `jakob__consumer` (
+      `consumerkey` varchar(20) COLLATE utf8_danish_ci NOT NULL,
+      `consumersecret` text COLLATE utf8_danish_ci NOT NULL,
+      `email` text COLLATE utf8_danish_ci NOT NULL,
+      PRIMARY KEY (`consumerkey`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;

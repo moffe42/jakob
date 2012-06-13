c35d8ac4cda197053292a00fae03c3fc7fa37f91
jach <jach@wayf.dk>
2012-06-13 11:27:12 +0000

    Origin information for all attributes are now saved in the session for later use

--- ssp-module/jakob/www/jakob.php
+++ ssp-module/jakob/www/jakob.php
@@ -1,9 +1,38 @@
 <?php
 // Restore session
+$session = SimpleSAML_Session::getInstance();
 $state = SimpleSAML_Auth_State::loadState($_POST['StateId'], 'jakob:request');
 
-// Grab the attributes returned from JAKOB
-$state['Attributes'] = json_decode($_POST['attributes'], true);
+// Grab attributes
+$attributes = $state['Attributes'];
+$jakob_attr = json_decode($_POST['attributes'], true);
+
+// Get authenticating IdP
+$idp = $session->getIdP();
+
+// build origin array
+$origin_attr = array();
+foreach ($attributes AS $key => $val) {
+	foreach ($val AS $v) {
+		$origin_attr[$key][sha1($v)] = $idp;
+	}	
+}
+
+// Merge attributes recived from JAKOB
+foreach ($jakob_attr AS $key => $val) {
+	foreach ($val AS $k => $v) {
+		if (!isset($attributes[$key]) || !in_array($v['value'], $attributes[$key])) {
+			$attributes[$key][] = $v['value'];
+			$origin_attr[$key][sha1($v['value'])] = isset($v['origin']) ? $v['origin'] : null;
+		}
+	}
+}
+
+// Save attributes to state
+$state['Attributes'] = $attributes;
+
+// Save origin info to session
+$session->setData('consent', 'origin', $orig_attr);
 
 // Resume processing
 SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);

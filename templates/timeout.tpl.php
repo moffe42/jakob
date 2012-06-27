66cf7f77017a18a93cd5918c6b704ff9d76acaa5
jach <jach@wayf.dk>
2012-06-27 07:14:27 +0000

    Updated the timeout template

--- templates/timeout.tpl.php
+++ templates/timeout.tpl.php
@@ -1,5 +1,7 @@
-<h1>Time out</h1>
+<h1>Zzzzzzz!!!</h1>
+<p>Det ser ud til at det tager lidt længere tid at hente alle dine oplysninger end forventet.</p>
+<p>Hav tålmodighed systemet arbejder. Tryk på knappen "Er den klar nu?"</p>
 <form method="post" action="">
 <input type="hidden" name="token" value="<?php echo $token; ?>" />
-    <input type="submit" value="Submit" />
+    <input type="submit" value="Er den klar nu?" />
 </form>

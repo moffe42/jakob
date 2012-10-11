8be00d68ec0d85de8e2e20ca99bf72012574eca6
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2012-10-11 10:35:34 +0000

    Added support for gracefull shutdown of worker

--- trunk/bin/connector-worker.php
+++ trunk/bin/connector-worker.php
@@ -1,5 +1,20 @@
 <?php
+declare(ticks=1);
 include '../www/_init.php';
+
+$stopwork = false;
+
+// Signal handler function
+function sig_handler($signo)
+{
+    switch ($signo) {
+        case SIGTERM:
+            // Workers is to terminate
+            $stopwork = true;
+            $logger->log(JAOKB_INFO, $connector_config['class'] . ' was forced to quit - SIGTERM');
+        }
+}
+pcntl_signal(SIGTERM, "sig_handler");
 
 try {
     $jakob_config = \WAYF\Configuration::getConfig();
@@ -35,9 +50,18 @@ if (isset($connector_config['class'])) {
     }
 }
 
+// Set timeout to 5 seconds
+$worker->setTimeout(5000);
+
 // Wait for work
-while($worker->work()) {
-    if ($$worker->_gworker->returnCode() != GEARMAN_SUCCESS) {
+while(@$gmworker->work() || $gmworker->returnCode() == GEARMAN_TIMEOUT) {
+    if ($worker->_gworker->returnCode() != GEARMAN_SUCCESS) {
         $logger->log(JAOKB_ERROR, $connector_config['class'] . ' did not return success on work. Return cod was: ', $worker->_gworker->returnCode());
     }
+    // Check to see if the workers have been killed
+    if ($stopwork) {
+        break;
+    }
 }
+
+$worker->unregisterAll()

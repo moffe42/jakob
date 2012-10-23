8220f865e43afab2a09a93737b1820e33c7c60e1
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2012-10-23 12:48:39 +0000

    Moved generic auth code to seperat file

--- trunk/www/admin/index2.php
+++ trunk/www/admin/index2.php
@@ -1,30 +1,6 @@
 <?php
 include '../_init.php';
-
-// Protection against session fixation attacks
-session_regenerate_id(true);
-
-$metadata = \WAYF\configuration::getConfig('adminauthmd.php');
-$users = \WAYF\configuration::getConfig('adminusers.php');
-
-function sporto($md, $providerids){
-	$res = new \WAYF\SAML();
-	if (isset($_POST['SAMLResponse'])) {
-		$res->receiveResponse($_POST['SAMLResponse'], $md);
-		return $res->getAttributes();
-	} else {
-		$res->sendRequest($providerids, $md);
-	}
-}
-
-if (!isset($_SESSION['loginok'])) {
-    $attributes = sporto($metadata, null);
-    if (in_array($attributes['eduPersonPrincipalName'][0], $users)) {
-        $_SESSION['loginok'] = true;
-    } else {
-        trigger_error('You are not authorized to access the JAKOB admin interface');
-    }
-}
+include 'auth.php';
 
 $actions = array(
     'list',

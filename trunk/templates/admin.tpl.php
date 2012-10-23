703b8e1a852b4a15d00d360e6566d358ba0fe473
jach@wayf.dk <jach@wayf.dk@fb6c9094-b5b9-ceb6-02c9-d257356f8b04>
2012-10-23 12:53:14 +0000

    Minor admin UI update

--- trunk/templates/admin.tpl.php
+++ trunk/templates/admin.tpl.php
@@ -4,5 +4,9 @@
     </head>
     <body>
         <h1>JAKOB - Admin</h1>
+        <ul>
+            <li><a href="admin-config.php">Configurations</a></li>
+            <li><a href="admin-config.php">Consumers</a></li>
+        </ul>
     </body>
 </html>

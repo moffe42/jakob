112717f34f002c9091367bc1bd4fd7b11a38dc21
jach <jach@wayf.dk>
2012-10-25 10:20:36 +0000

    fixed syntax error in translation file

--- translation/main.php
+++ translation/main.php
@@ -7,19 +7,19 @@ $translation = array(
         'en' => 'JAKOB',
         'da' => 'JAKOB',
     ),
-    'TIMEOUT_TITLE' array(
+    'TIMEOUT_TITLE' => array(
         'da' => 'Vi er næsten klar',
         'en' => 'We are almost ready',    
     ),
-    'TIMEOUT_DESC1' array(
+    'TIMEOUT_DESC1' => array(
         'da' => 'Det ser ud til at det tager lidt længere tid end forventet at hente alle dine oplysninger.',
         'en' => 'Look like it will take a little while longer to fetch your informations.',    
     ),
-    'TIMEOUT_DESC2' array(
+    'TIMEOUT_DESC2' => array(
         'da' => 'Hav tålmodighed systemet arbejder. Tryk på knappen "Er den klar nu?',
         'en' => 'Please have patience while the system is working. Press the button "Is it ready now?"',    
     ),
-    'TIMEOUT_BUTTON' array(
+    'TIMEOUT_BUTTON' => array(
         'da' => 'Er den klar nu?',
         'en' => 'Is it ready now?',    
     ),

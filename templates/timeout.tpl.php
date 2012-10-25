2ca0cdd74ef03249c79344647f96078ccc33ef52
jach <jach@wayf.dk>
2012-10-25 10:17:56 +0000

    Set up translation of timeout template

--- templates/timeout.tpl.php
+++ templates/timeout.tpl.php
@@ -1,8 +1,7 @@
-<h1><?= $this->t('TITLE') ?></h1>
-<h1>Zzzzzzz!!!</h1>
-<p>Det ser ud til at det tager lidt længere tid at hente alle dine oplysninger end forventet.</p>
-<p>Hav tålmodighed systemet arbejder. Tryk på knappen "Er den klar nu?"</p>
+<h1><?= $this->t('TIMEOUT_TITLE') ?></h1>
+<p><?= $this->t('TIMEOUT_DESC1') ?></p>
+<p><?= $this->t('TIMEOUT_DESC2') ?></p>
 <form method="post" action="">
 <input type="hidden" name="token" value="<?php echo $token; ?>" />
-    <input type="submit" value="Er den klar nu?" />
+    <input type="submit" value="<?= $this->t('TIMEOUT_BUTTON') ?>" />
 </form>
